a6e3f354033a11d2e2271f32523b5dcf17c141b6
aamirta <aamirtahtah@gmail.com>
2023-09-08 13:01:48 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet
+ *
+ * Return: 0 (seccess)
+*/
+
+int main(void)
+{
+	int  alpha = '0';
+
+	char ti = 'a';
+
+	while (alpha <= '9')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	while (ti <= f)
+	{
+		putchar(ti);
+		ti++;
+	}
+	putchar('\n');
+	return (0);
+}
