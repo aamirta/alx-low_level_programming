f4b3bc8ffbd1cc6484674a40407a5c9a35448081
aamirta <aamirtahtah@gmail.com>
2023-09-08 13:07:46 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet
+ *
+ * Return: 0 (seccess)
+*/
+
+int main(void)
+{
+	int  alpha = '0';
+
+	while (alpha <= '9')
+	{
+		putchar(alpha);
+		alpha++;
+		putchat(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
