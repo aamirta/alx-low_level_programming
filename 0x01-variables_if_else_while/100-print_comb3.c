f5e7bfd4fc67ea09cb2d620437cda2a2d71239cc
aamirta <aamirtahtah@gmail.com>
2023-09-08 13:38:06 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet
+ *
+ * Return: 0 (seccess)
+*/
+int main(void)
+{
+	int degit1 = 0, degit2;
+
+	while (degit <= 9)
+	{
+		degit2 = 0;
+			while (degit2 <= 9)
+			{
+				if (degit1 != degit2 && degit1 < degit2)
+				{
+					putchar(degit1 + 48);
+					putchar(degit2 + 48);
+					if (degit1 + degit2 != 17)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				degit2++;
+			}
+			degit1++;
+	}
+	putchar('\n');
+	return (0);
+}
+
