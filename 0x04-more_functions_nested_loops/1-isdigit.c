46cc6426f559e4d7c92b09c6e418d25e0826b4ec
aamirta <aamirtahtah@gmail.com>
2023-09-15 13:04:10 -0700

    a

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,11 @@
  *
  * @c: input
  *
- * Return: 1 if is digit 0 if not 
+ * Return: 1 if is digit 0 if not
 */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 97 )
+	if (c >= 48 && c <= 97)
 		return (1);
 	else
 		return (0);
